f1be450ea92dcc3aec47ab46cd3b1545e9a4a92c
Plakim007 <31442234+Plakim007@users.noreply.github.com>
2017-09-04 19:54:53 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,7 @@
 <?php
 $access_token = 'k8hkSyMu4vTCrKMbHGHT7Mnr6SH0pSJTrDPDxVQlwHnaZw7WPbXnCFUE8iJ++3lJ9ZkwEDvyEfj89a87FDKGnVegGezaJCAclT+3r85iwSafB/oJAm1KUGfHEFlcUxxQS28VBGrDf5xQXAZY9Vyd/gdB04t89/1O/w1cDnyilFU=';
-
+$proxy = 'proxyurl:http://fixie:CMgHptyxrvJFJF7@velodrome.usefixie.com:80';
+$proxyauth = 'username:http://fixie:CMgHptyxrvJFJF7@velodrome.usefixie.com:80';
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
@@ -9,6 +10,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
